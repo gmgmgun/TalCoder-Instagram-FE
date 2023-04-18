459ace758c5618adfd50a773085b209d28d30a58
dongminlee <gmgmgun@gmail.com>
2023-04-18 20:16:41 +0900

    :loud_sound: Add: Outlet for Hiding Nav on SignUp & SignIn
    :bug: Fix: Outlet & Router Code
    
    :bug: Fix: import Code

--- src/components/OutletContainer/OutletContainer.tsx
+++ src/components/OutletContainer/OutletContainer.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import TopNav from '../Nav/Nav';
+import Nav from '../Nav/Nav';
 
 const OutletContainer = () => {
   return (
     <>
-      <TopNav />
+      <Nav />
       <Outlet />
     </>
   );
