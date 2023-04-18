6f85035400c536a476c6ecf9d81dd9c4020ad328
dongminlee <gmgmgun@gmail.com>
2023-04-18 20:16:41 +0900

    :loud_sound: Add: Outlet for Hiding Nav on SignUp & SignIn
    :bug: Fix: Outlet & Router Code
    
    :bug: Fix: import Code
    
    :construction: Add: 회원가입 페이지 기본 레이아웃
    
    :construction: Add: onChange 이벤트 처리, userInfo State
    
    :lipstick: Fix: UI
    
    :lipstick: Add: SignIn Style

--- src/pages/SignUp/SignUpStyle.tsx
+++ src/pages/SignUp/SignUpStyle.tsx
@@ -1,3 +1,75 @@
 import styled from 'styled-components';
 
-export const SignUp = styled.div``;
+export const SignUp = styled.div`
+  width: 100%;
+  // height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const SignUpWrap = styled.div`
+  max-width: 350px;
+  width: 100%;
+  padding: 30px;
+  background-color: #ffffff;
+  border: 1px solid #dbdbdb;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 2rem;
+`;
+
+export const Text = styled.p`
+  font-size: 1.5rem;
+  color: #8e8e8e;
+  text-align: center;
+  margin-bottom: 1.5rem;
+`;
+
+export const SignUpForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const Input = styled.input`
+  padding: 10px;
+  border: 1px solid #dbdbdb;
+  margin-bottom: 1rem;
+  font-size: 1rem;
+`;
+export const ButtonSubmit = styled.button`
+  background-color: #0095f6;
+  color: #ffffff;
+  border: none;
+  padding: 10px;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  cursor: pointer;
+`;
+
+export const GoToSignInWrap = styled(SignUpWrap)`
+  margin-top: 1rem;
+  flex-direction: row;
+`;
+
+export const TextGoToSignIn = styled.p`
+  font-size: 1.3rem;
+`;
+
+export const ButtonGoToSignIn = styled.button`
+  background-color: transparent;
+  color: #0095f6;
+  border: none;
+  font-size: 1.3rem;
+  margin-left: 0.5rem;
+  cursor: pointer;
+`;
